d296386a311e52753086d5674c8fcb78f6e8fe3d
Tilden <tildenwinston@gmail.com>
2019-03-24 05:48:55 -0400

    Giving up on on-site snapchat login

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,5 +17,47 @@ export class AppComponent {
 
   ngOnInit(){
 
+    
+
+  /* 
+(function (d, s, id) {
+  var js, sjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://sdk.snapkit.com/js/v1/login.js";
+  sjs.parentNode.insertBefore(js, sjs);
+}(document, 'script', 'loginkit-sdk'));
+
+
+(<any>window).snapKitInit = function () {
+    var loginButtonIconId = 'my-login-button-target';
+    // Mount Login Button
+    
+    snap.loginkit.mountButton(loginButtonIconId, {
+      clientId: '1a9fe673-920e-49ce-9735-2ddaada657c6',
+      redirectURI: 'http://localhost:4200/',
+      //redirectURI: 'https://api-project-1027665890840.firebaseapp.com/profile',
+      scopeList: [
+        'user.display_name',
+        'user.bitmoji.avatar'
+      ],
+      handleResponseCallback: function() {
+        snap.loginkit.fetchUserInfo()
+          .then(data => {
+            console.log('User info:', data);
+        });
+      },
+    });
+    
   };
+ // Load the SDK asynchronously
+(function (d, s, id) {
+    var js, sjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://sdk.snapkit.com/js/v1/login.js";
+    sjs.parentNode.insertBefore(js, sjs);
+  }(document, 'script', 'loginkit-sdk'));
+  */
+  }; 
 }
